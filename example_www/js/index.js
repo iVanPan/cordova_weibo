c912dab6bd00031c375972e3de51b411b58972ac
Van <fan543119@gmail.com>
2015-07-01 10:15:15 +0800

    add www example

---
+++ example_www/js/index.js
@@ -0,0 +1,37 @@
+var app = function () {
+  this.checkClientInstalled = function () {
+    YCWeibo.checkClientInstalled(function(){
+      alert('client is installed');
+    },function(){
+      alert('client is not installed');
+    });
+  };
+  this.ssoLogin = function () {
+    YCWeibo.ssoLogin(function(args){
+     alert(args.access_token);
+     alert(args.userid);
+   },function(failReason){
+     alert(failReason);
+   });
+  };
+  this.shareToWeibo = function () {
+    var args = {};
+    args.url = "http://www.baidu.com";
+    args.title = "Baidu";
+    args.description = "This is Baidu";
+    args.imageUrl = "https://www.baidu.com/img/bdlogo.png";//if you don't have imageUrl,for android http://www.sinaimg.cn/blog/developer/wiki/LOGO_64x64.png will be the defualt one
+    args.defaultText = "";
+    YCWeibo.shareToWeibo(function () {
+      alert("share success");
+    }, function (failReason) {
+      alert(failReason);
+    }, args);
+  };
+  this.logout = function () {
+    YCWeibo.logout(function(){
+      alert('logout success');
+    },function(failReason){
+      alert(failReason);
+    });
+  }
+}

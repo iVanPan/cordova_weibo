fc6ee566cfb66251f82d13b2c477dbd5893b9ac3
Van <fan543119@gmail.com>
2017-04-18 13:13:14 +0800

    update demo

--- example_www/js/index.js
+++ example_www/js/index.js
@@ -1,35 +1,37 @@
 var app = function () {
   this.checkClientInstalled = function () {
-    YCWeibo.checkClientInstalled(function(){
+    WeiboSDK.checkClientInstalled(function(){
       alert('client is installed');
     },function(){
       alert('client is not installed');
     });
   };
+
   this.ssoLogin = function () {
-    YCWeibo.ssoLogin(function(args){
-     alert("access token is "+args.access_token);
-     alert("userid is "+args.userid);
+    WeiboSDK.ssoLogin(function(args){
+      alert("access token is "+args.access_token);
+      alert("userid is "+args.userid);
       alert("expires_time is "+ new Date(parseInt(args.expires_time)) + " TimeStamp is " +args.expires_time);
    },function(failReason){
-     alert(failReason);
+      alert(failReason);
    });
   };
+
   this.shareToWeibo = function () {
     var args = {};
-    args.url = "http://www.baidu.com";
-    args.title = "Baidu";
-    args.description = "This is Baidu";
-    args.imageUrl = "https://www.baidu.com/img/bdlogo.png";//if you don't have imageUrl,for android http://www.sinaimg.cn/blog/developer/wiki/LOGO_64x64.png will be the defualt one
-    args.defaultText = "";
-    YCWeibo.shareToWeibo(function () {
+    args.url = "https://cordova.apache.org/";
+    args.title = "Apache Cordova";
+    args.description = "This is a Cordova Plugin";
+    args.image = "https://cordova.apache.org/static/img/pluggy.png"; 
+    WeiboSDK.shareToWeibo(function () {
       alert("share success");
     }, function (failReason) {
       alert(failReason);
     }, args);
   };
+
   this.logout = function () {
-    YCWeibo.logout(function(){
+    WeiboSDK.logout(function(){
       alert('logout success');
     },function(failReason){
       alert(failReason);

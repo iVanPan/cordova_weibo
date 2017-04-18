6d78b86562f9985f9bcfbbbf094cbd6aab00d3e7
Van <fan543119@gmail.com>
2017-04-18 13:13:05 +0800

    update plugin src code

--- src/android/WeiboSDKShareCallback.java
+++ src/android/WeiboSDKShareCallback.java
@@ -1,38 +1,36 @@
-package org.zy.yuancheng.weibo;
-
+package me.vanpan.weibosdk;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.sina.weibo.sdk.api.share.BaseResponse;
 import com.sina.weibo.sdk.api.share.IWeiboHandler;
 import com.sina.weibo.sdk.api.share.WeiboShareSDK;
 import com.sina.weibo.sdk.constant.WBConstants;
 
-public class YCWeiboShareCallback extends Activity implements IWeiboHandler.Response {
+public class WeiboSDKShareCallback extends Activity implements IWeiboHandler.Response {
 
 	private static final String CANCEL_BY_USER ="cancel by user";
 	private static final String SHARE_FAIL ="sharefail";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		YCWeibo.mWeiboShareAPI = WeiboShareSDK.createWeiboAPI(this,YCWeibo.APP_KEY);
-		YCWeibo.mWeiboShareAPI.registerApp();
-        YCWeibo.mWeiboShareAPI.handleWeiboResponse(getIntent(), this);
+		WeiboSDKPlugin.mWeiboShareAPI = WeiboShareSDK.createWeiboAPI(this,WeiboSDKPlugin.APP_KEY);
+		WeiboSDKPlugin.mWeiboShareAPI.registerApp();
+		WeiboSDKPlugin.mWeiboShareAPI.handleWeiboResponse(getIntent(), this);
 	}
 
 	@Override
 	public void onResponse(BaseResponse baseResp) {
 		switch (baseResp.errCode) {
 		case WBConstants.ErrorCode.ERR_OK:
-			YCWeibo.currentCallbackContext.success();
+			WeiboSDKPlugin.currentCallbackContext.success();
 			break;
 		case WBConstants.ErrorCode.ERR_CANCEL:
-			YCWeibo.currentCallbackContext.error(CANCEL_BY_USER);
+			WeiboSDKPlugin.currentCallbackContext.error(CANCEL_BY_USER);
 			break;
 		case WBConstants.ErrorCode.ERR_FAIL:
-			YCWeibo.currentCallbackContext.error(SHARE_FAIL);
+			WeiboSDKPlugin.currentCallbackContext.error(SHARE_FAIL);
 			break;
 		}
 		this.finish();
@@ -41,7 +39,7 @@ public class YCWeiboShareCallback extends Activity implements IWeiboHandler.Resp
 	@Override
 	public void onNewIntent(Intent intent) {
 		super.onNewIntent(intent);
-		YCWeibo.mWeiboShareAPI.handleWeiboResponse(intent, this);
+		WeiboSDKPlugin.mWeiboShareAPI.handleWeiboResponse(intent, this);
 	}
 
 }
